68503b496bde43535d8259256c116bf0c8cc2b78
rudast <ruslan5haertdinov@yandex.ru>
2026-06-18 02:00:59 +0300

    feat: add alembic settings and initial migration

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,3 +3,9 @@ from sqlalchemy.orm import DeclarativeBase
 
 class Base(DeclarativeBase):
     pass
+
+
+from app.models.user import UserModel
+from app.models.task import TaskModel
+
+__all__ = ["Base", "UserModel", "TaskModel"]
